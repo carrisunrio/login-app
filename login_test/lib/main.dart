84ea1e753ee1edff40f0b2918946203fc4304dc6
Riya Suntwal <carrisun91rio@gmail.com>
2020-12-11 12:31:29 +0530

    Added Updated Code

--- login_test/lib/main.dart
+++ login_test/lib/main.dart
@@ -18,7 +18,26 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin{
+
+  AnimationController _iconAnimationController;
+  Animation<double> _iconAnimation;
+
+  @override
+  void initState(){
+    super.initState();
+    _iconAnimationController = new AnimationController(
+      vsync: this,
+      duration: new Duration(milliseconds: 500)
+    );
+    _iconAnimation = new CurvedAnimation(
+      parent: _iconAnimationController,
+      curve: Curves.bounceOut
+    );
+    _iconAnimation.addListener(() => this.setState((){}));
+    _iconAnimationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,9 +45,18 @@ class _LoginPageState extends State<LoginPage> {
       body: new Stack(
         fit: StackFit.expand,
         children: <Widget>[
+          new Image(
+            image: new AssetImage("assets/read.png"),
+            fit: BoxFit.cover,
+            color: Colors.black87,
+            colorBlendMode: BlendMode.darken,
+            ),
           new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              new FlutterLogo(
+                size: _iconAnimation.value * 100,
+              ),
               new Form(
                 child: new Theme(
                   data: ThemeData(
